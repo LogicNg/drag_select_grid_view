7b6d43198f38107fc4f9c4301d683201558595c0
Ng Lok Chun <nglokchun@gmail.com>
2024-04-30 00:18:47 +0800

    Initial commit

---
+++ example/lib/example.dart
@@ -0,0 +1,76 @@
+import 'package:drag_select_grid_view/drag_select_grid_view.dart';
+import 'package:flutter/material.dart';
+
+import 'selectable_item.dart';
+import 'selection_app_bar.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: const MyApp(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(elevation: 2),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = DragSelectGridViewController();
+  final sc = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(scheduleRebuild);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(scheduleRebuild);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: SelectionAppBar(
+        selection: controller.value,
+        title: const Text('Grid Example'),
+      ),
+      body: DragSelectGridView(
+        crossAxisCount: 7,
+        scrollController: sc,
+        shrinkWrap: true,
+        gridController: controller,
+        itemCount: 24 * 2 * 7,
+        itemBuilder: (context, index, selected) {
+          return SelectableItem(
+            index: index,
+            color: Colors.blue,
+            selected: selected,
+          );
+        },
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 7,
+          childAspectRatio: 4 / 3,
+          crossAxisSpacing: 1,
+          mainAxisSpacing: 1,
+        ),
+      ),
+    );
+  }
+
+  void scheduleRebuild() => setState(() {});
+}
